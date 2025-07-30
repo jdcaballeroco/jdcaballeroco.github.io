941b08eebf13095f04cd17d8a09af72e804707cd
Jesus Caballero <62200154+jdcaballeroco@users.noreply.github.com>
2025-07-29 23:17:48 -0500

    Actualizacion automatica - 2025-29-07

--- scripts/commit_fecha.bat
+++ scripts/commit_fecha.bat
@@ -1,5 +1,5 @@
 @echo off
 set FECHA=%date:~10,4%-%date:~4,2%-%date:~7,2%
 git add .
-git commit -m "Actualización automática - %FECHA%"
+git commit -m "Actualizacion automatica - %FECHA%"
 git push
